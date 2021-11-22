b71c0422f856b050559c7f5ad5584c3f58f5a667
Bastiaan van Hoorn <bastiaan97.bvh@gmail.com>
2021-11-22 14:33:18 +0100

    added some logging

--- Assignment 1/Part 3/quicksort.m
+++ Assignment 1/Part 3/quicksort.m
@@ -2,6 +2,10 @@ function quicksort(left,right)
     global list
     if (right > left)
         i = partition(left,right);
+        i
+        left
+        right
+        list
         quicksort(left,i-1);
         quicksort(i+1,right);
     end % if
